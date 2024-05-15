f3145ba111f50012593def77c85ba76c32114513
R4H4T69 <166297407+R4H4T69@users.noreply.github.com>
2024-05-15 22:13:18 +0600

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -11,10 +11,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 LOGS = logging.getLogger(__name__)
 
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('API_ID', CONFIG['api_id']))
-API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
-DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
-STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
+API_ID = int(os.getenv('API_ID', CONFIG['25135353']))
+API_HASH = os.getenv('API_HASH', CONFIG['744ef666a3409e3069cbe0e8e6ef30ba'])
+DUMP_ID = os.getenv('DUMP_ID', CONFIG['-1002057654379'])
+STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['1BVtsOJEBu20w56yWzZw68t0o75q7BKeaydFcZ65WQhOAyYc3NxgJ3vOSg72BAY5tXK-zZ-igbBgP4dELH0K7gxH6yjWVWP6brbWerBGN1ytHPhr_jwEGZN8LSrHlhUHJyBVKwg_caLoWidI1RKVVanNrey54w2rD8b4OScxMQUunwVGl3jDN1S-Ty7wl5SDdpwspqbrK-ZW0X-XBrf3qpKJOVBOjpDzkfoYeQm4o9ppO-wD11zYCpL4WwmWleb5IZ9404k67GRYPpMF7K_PwRzTdnoy_xHGZp4MftgUkMFgjoSRUcDiH0oGfCck8qgarmOu62uYzdKg5t_tAgfuGcb0S8mcmCKY='])
 
 Ubot = TelegramClient(StringSession(STRING_SESSION),
                       API_ID,
